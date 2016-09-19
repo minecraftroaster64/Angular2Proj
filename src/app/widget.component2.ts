8c3040d55c1d827cbe722b940305fea8c7d1d95d
nks <nks>
2016-09-18 19:24:10 -0700

    first commit

---
+++ src/app/widget.component2.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by ajit on 9/17/2016.
+ */
+import {Component} from "@angular/core";
+@Component({
+    selector: 'skp-img',
+    template: `<img src="https://i.ytimg.com/vi/VCs0hkL52WY/maxresdefault.jpg" alt="Subject #1: Dank Memes: Class B">`
+})
+
+export class SkinnyPComponent {};
